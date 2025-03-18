098765351c56a0c8b434ecddac09ea7e0dde72e9
mikelgaray <garaymikelaritz@gmail.com>
2025-03-18 13:38:01 +0100

    creacion clases

--- src/model/Games.java
+++ src/model/Games.java
@@ -1,5 +1,22 @@
 package model;
 
 public class Games {
+	private String nombre;
 
+	public Games() {
+		this.nombre="";
+	}
+	public Games(String nom) {
+		this.nombre=nom;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	@Override
+	public String toString() {
+		return "Trophy [nombre=" + nombre + "]";
+	}
 }
--- src/model/Player.java
+++ src/model/Player.java
@@ -1,5 +1,49 @@
 package model;
 
 public class Player {
+	private String nombre;
+	private String password;
+	private int points;
+	private int id;
 
+	public Player() {
+		this.nombre="";
+		this.password="";
+		this.points=0;
+		this.id=0;
+	}
+	public Player(String nom, String pass, int point, int id) {
+		this.nombre=nom;
+		this.password=pass;
+		this.points=point;
+		this.id=id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getPoints() {
+		return points;
+	}
+	public void setPoints(int points) {
+		this.points = points;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	@Override
+	public String toString() {
+		return "Player [nombre=" + nombre + ", password=" + password +", id= "+ id + ", points= " + points + "]";
+	}
 }
