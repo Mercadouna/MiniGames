1c15a9de9366a84792bc77c6b3cdf77ecaa223ff
“AKGM17” <ekaintorresmiranda@gmail.com>
2025-03-21 12:28:47 +0100

    phinal fush

--- src/model/PlayerDAO.java
+++ src/model/PlayerDAO.java
@@ -2,11 +2,11 @@ package model;
 
 public interface PlayerDAO {
 
-	public boolean comprobarplayer(Player player);
-	public boolean eliminarplayer(Player player);
+	public boolean compareplayer(Player player);
 	public boolean modificarpuntos(Player player);
-	public boolean eliminarhist(Player player);
 	public boolean visualizarhist(Player player);
+	public void deleteplayer(Player player);
+	boolean eliminarhist(Player player);
 
 
 }
