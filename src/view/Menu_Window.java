f05bc5be8af2820c2cb52236410cee3323e3ab61
Mercadouna <20alain.martinez05@gmail.com>
2025-03-28 09:41:53 +0100

    update

--- src/view/Menu_Window.java
+++ src/view/Menu_Window.java
@@ -16,24 +16,31 @@ import javax.swing.ImageIcon;
 import java.util.random.*;
 
 public class Menu_Window extends JFrame  implements ActionListener{
-
-	private JPanel contentPane;
-	private LoginControlador cont;
-	private JButton btnGame_2_math;
-	private JButton btnTrophyRoom;
-	private JButton btnGame_1_aim;
-	private Player j;
 	
 	/**
 	 * Create the frame.
 	 * @param j 
 	 * @param cont 
 	 */
+	
+	private JPanel contentPane;
+	private LoginControlador cont;
+	private JButton btnGame_2_math;
+	private JButton btnTrophyRoom;
+	private JButton btnGame_1_aim;
+	private JButton btnRecord;
+	private JButton btnDelete;
+	private JButton btnLogOut;
+	private Player j;
+	/**
+	 * Create the frame.
+	 * @param cont 
+	 */
 	public Menu_Window(LoginControlador controler, Player j) {
 		this.cont =controler;
 		this.j = j;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 843, 521);
+		setBounds(100, 100, 570, 539);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -41,22 +48,42 @@ public class Menu_Window extends JFrame  implements ActionListener{
 		contentPane.setLayout(null);
 		
 		btnGame_1_aim = new JButton("Aim Game");
+		btnGame_1_aim.setBounds(56, 118, 126, 143);
 		btnGame_1_aim.setIcon(new ImageIcon("C:\\Users\\20ala\\Downloads\\Logo+Mercadona+Web.png"));
-		btnGame_1_aim.setBounds(53, 104, 126, 143);
 		contentPane.add(btnGame_1_aim);
 		btnGame_1_aim.addActionListener(this);
 		
 		btnTrophyRoom = new JButton("Trophy room");
-		btnTrophyRoom.setBounds(533, 377, 261, 57);
+		btnTrophyRoom.setBounds(133, 335, 261, 57);
 		contentPane.add(btnTrophyRoom);
 		btnTrophyRoom.addActionListener(this);
 		
 		btnGame_2_math = new JButton("Math Game");
-		btnGame_2_math.setBounds(310, 104, 126, 143);
+		btnGame_2_math.setBounds(324, 118, 126, 143);
 		contentPane.add(btnGame_2_math);
+		
+		btnRecord = new JButton("Record");
+		btnRecord.setBounds(25, 37, 126, 34);
+		contentPane.add(btnRecord);
+		btnRecord.addActionListener(this);
+		
+		btnDelete = new JButton("Delete user");
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnDelete.setBounds(409, 37, 112, 34);
+		contentPane.add(btnDelete);
+		btnDelete.addActionListener(this);
+		
+		btnLogOut = new JButton("Log out");
+		btnLogOut.setBounds(25, 471, 85, 21);
+		contentPane.add(btnLogOut);
+		btnLogOut.addActionListener(this);
 		btnGame_2_math.addActionListener(this);
 	}
 
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
